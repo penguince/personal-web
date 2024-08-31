93c7cc40556d770de45367cb4d4f499ddb1a5038
penguince <xiajieou.yc@gmail.com>
2024-08-31 02:19:03 -0400

    icon

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -10,18 +10,17 @@ const Main = ({ children, router }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Xia Jie Ou's homepage" />
         <meta name="author" content="Xia Jie Ou" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/images/penguinforicon.webp" type="image/webp" />
         <meta property="og:site_name" content="Xia Jie Ou" />
         <meta name="og:title" content="Xia Jie Ou" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        <meta property="og:image" content="/images/penguinforicon.webp" />
         <title>Xia Jie Ou - Homepage</title>
       </Head>
 
       <NavBar path={router.asPath} />
 
       <Container maxW="container.md" pt={14}>
-        {/* Removed the LazyVoxelDog component */}
         {children}
 
         <Footer />
